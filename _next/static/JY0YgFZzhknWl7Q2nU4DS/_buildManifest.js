beddbc2408c616e8fff76b425e56f6a495201ffc
GitHub-ccd <GitHub-ccd@users.noreply.github.com>
2026-06-21 15:06:10 +0000

    deploy: c62eab97fbea45cccffbcbd0f1c6278a26a7366f

--- _next/static/JY0YgFZzhknWl7Q2nU4DS/_buildManifest.js
+++ _next/static/JY0YgFZzhknWl7Q2nU4DS/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/3i46p59g2rj6r.js"
+    "static/chunks/0s_4bp80hjw6w.js"
   ],
   "/_error": [
     "static/chunks/1m5kfhygr-4jd.js"
--- _next/static/chunks/3i46p59g2rj6r.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/02qrpuoloi1z7.js",
-  "static/chunks/0zjghqtnmrnuc.js",
-  "static/chunks/turbopack-1voj_juzz-3b4.js"
-])
